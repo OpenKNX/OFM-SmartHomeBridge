c28a3496e4f105884393ccb83c9e8c2c227b0631
Michael Geramb <lasso_riefen0m@icloud.com>
2025-06-23 21:11:35 +0200

    Remove version info from showInformations

--- src/SmartHomeBridgeModule.cpp
+++ src/SmartHomeBridgeModule.cpp
@@ -35,11 +35,6 @@ const std::string SmartHomeBridgeModule::name()
 
 void SmartHomeBridgeModule::showInformations()
 {
-#ifdef MODULE_SmartHomeBridge_Version
-  openknx.logger.logWithPrefixAndValues(logPrefix(), "Smart Home Bridge %s", MODULE_SmartHomeBridge_Version);
-#else
-  openknx.logger.logWithPrefix(logPrefix(), "Smart Home Bridge");
-#endif
 }
 
 const std::string SmartHomeBridgeModule::version()
