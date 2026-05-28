5efb9e1784c34b3e31bf571b1f281f16665a15d8
Michael Geramb <lasso_riefen0m@icloud.com>
2026-05-28 22:25:54 +0200

    Display Widget detail view

--- src/Display/WebVisuDisplay.cpp
+++ src/Display/WebVisuDisplay.cpp
@@ -46,13 +46,17 @@ std::string WebVisuDisplay::webVisuOverviewHtml(uint8_t channelIndex) const
 
 std::string WebVisuDisplay::webVisuDetailHtml(uint8_t channelIndex) const
 {
-    const std::string value = _hasText ? _textValue : std::to_string(_value);
+    const std::string value = _channel != nullptr
+                                  ? _channel->currentValueAsString()
+                                  : (_hasText ? _textValue : std::to_string(_value));
     return renderCard(channelIndex, _name, "Display", value, "");
 }
 
 std::string WebVisuDisplay::webVisuJson(uint8_t channelIndex) const
 {
-    const std::string value = _hasText ? _textValue : std::to_string(_value);
+    const std::string value = _channel != nullptr
+                                  ? _channel->currentValueAsString()
+                                  : (_hasText ? _textValue : std::to_string(_value));
     std::string json = "{";
     json += "\"kind\":\"display\",";
     json += "\"channel\":" + std::to_string((int)channelIndex + 1) + ",";
